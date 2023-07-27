f5c5260ee682df41f6d6d6d0ac0ef961968fc49b
FLOYD <sungmin.park5941@gmail.com>
2023-07-27 15:34:31 +0900

    #3-1 플레이어 이동 기초

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Local -> World
+        //TransformDirection
+
+        //World -> Local
+        //InverseTransformDirection
+
+        //바라보는 기준으로 계산
+        //Translate
+    }
+}
