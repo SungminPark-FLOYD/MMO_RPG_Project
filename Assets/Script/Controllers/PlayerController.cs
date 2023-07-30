699cb3b2f33e69cbc30fab8d406fbf210bdc624b
FLOYD <sungmin.park5941@gmail.com>
2023-07-30 15:40:10 +0900

    #3-5 Animation

--- Assets/Script/Controllers/PlayerController.cs
+++ Assets/Script/Controllers/PlayerController.cs
@@ -8,47 +8,79 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float _speed = 10.0f;
 
-    bool _moveToDest = false;
+    //bool _moveToDest = false;
     Vector3 _destPos;
     void Start()
     {
         //구독신청
-        Managers.Input.KeyAction -= OnKeyBoard;
-        Managers.Input.KeyAction += OnKeyBoard;
+        //Managers.Input.KeyAction -= OnKeyBoard;
+        //Managers.Input.KeyAction += OnKeyBoard;
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
 
     }
 
-    void Update()
+   
+    public enum PlayerState
     {
-        //Local -> World
-        //TransformDirection
-
-        //World -> Local
-        //InverseTransformDirection
-
-        //바라보는 기준으로 계산
-        //Translate
-        
-        if(_moveToDest)
-        {
-            Vector3 dir = _destPos - transform.position;
-            if (dir.magnitude < 0.0001f)
-            {
-                _moveToDest = false;
-            }
-            else
-            {
-                //Mathf.Clamp 를 이용해서 거리계산에 오류가 없도록 예방
-                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-                transform.position += dir.normalized * moveDist;
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
-
-            }
-        }
+        Die,
+        Moving,
+        Idle,
     }
 
+    PlayerState _state = PlayerState.Idle;
+
+    void UpdateDie()
+    {
+
+    }
+    void UpdateMoving()
+    {
+        Vector3 dir = _destPos - transform.position;
+        if (dir.magnitude < 0.0001f)
+        {
+            _state = PlayerState.Idle;
+        }
+        else
+        {
+            //Mathf.Clamp 를 이용해서 거리계산에 오류가 없도록 예방
+            float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+            transform.position += dir.normalized * moveDist;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 20 * Time.deltaTime);
+
+        }
+
+        //애니메이션
+        Animator anim = GetComponent<Animator>();
+        //현재 게임 상태에 대한 정보를 넘겨준다
+        anim.SetFloat("speed", _speed);
+    }
+    void UpdateIdle()
+    {
+        //애니메이션
+        Animator anim = GetComponent<Animator>();
+        //현재 게임 상태에 대한 정보를 넘겨준다
+        anim.SetFloat("speed", 0);
+    }
+    void Update()
+    {                      
+
+        switch (_state)
+        {
+            case PlayerState.Die:
+                UpdateDie();
+                break;
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+        }
+           
+    }
+    //키보드 입력
+    /*
     void OnKeyBoard()
     {
         if(Input.GetKey(KeyCode.W))
@@ -74,10 +106,13 @@ public class PlayerController : MonoBehaviour
 
         _moveToDest = false;
     }
+    */
 
     void OnMouseClicked(Define.MouseEvent evt)
     {
-        if (evt != Define.MouseEvent.Click) return;
+        //if (evt != Define.MouseEvent.Click) return;
+
+        if (_state == PlayerState.Die) return;
 
         //레이저 그리기
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -88,11 +123,27 @@ public class PlayerController : MonoBehaviour
         if(Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
         {
             _destPos = hit.point;
-            _moveToDest = true;
-            //Debug.Log($"Raycase Camera @ {hit.collider.gameObject.tag}");
+            _state = PlayerState.Moving;
         }
 
     }
 
     
 }
+/*
+ *       Local -> World
+ *       TransformDirection
+ *
+ *       World -> Local
+ *       InverseTransformDirection
+ *
+ *       바라보는 기준으로 계산
+ *       Translate     
+ *
+ * 상태 패턴
+ * 매번 애니메이션을 집어넣게 되면 규모가 커졌을 때 관리하기 힘들기 때문에 상태패턴으로 관리한다
+ * 하지만 한번에 하나의 상태만 가질수 있기 때문에 움직이면서 스킬을 쓰는 것과 같이 특수한 경우에서는 다른 방법을 찾아야 한다
+ *   
+ * 
+*/
+
